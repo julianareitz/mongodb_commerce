f3147c3782d94071310a5d432b5a958098230ffb
juliana.reitz <juliana.mreitz@gmail.com>
2023-02-07 15:29:58 -0300

    feat - Req12

---
+++ challenges/desafio12.js
@@ -0,0 +1,12 @@
+db.produtos.updateMany(
+  { nome: { $ne: "McChicken" } },
+  { $addToSet: { ingredientes: "ketchup" } },
+)
+
+
+db.produtos.find(
+  {},
+  { nome: 1,
+    _id: 0,
+    ingredientes: 1
+  });
